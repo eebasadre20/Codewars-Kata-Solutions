ee771ca0ce0f27e133fbc38c6f7b2f5661697211
eebasadre <edsil@piktochart.com>
2016-10-08 19:53:27 +0800

    Refactor and fix the failing test in number-wait-letter

--- number-wait-letters.rb
+++ number-wait-letters.rb
@@ -1,46 +1,60 @@
 def do_math(s)
-    split_string = s.split(' ')
-    operation = DoTheOperation.new( split_string )
+    splitted_str = s.split(' ')
+    operation = DoTheOperation.new( splitted_str )
     operation.do_math
 end
 
 class DoTheOperation
   OPERATIONS = ['+', '-', '*', '/']
 
-  def initialize( split_string )
-    @split_string = split_string
-    @letter = []
-    @num = []
+  def initialize( splitted_str )
+    @splitted_str = splitted_str
+    @char_list = []
+    @num_list = []
   end
 
   def do_math
     total = 0
-    @split_string.each do | a |
-      extract_array( a )
-      do_sorting
+    splitted_str.each do | a |
+      extract_str( a )
     end
 
-    output
-    total = do_calculation
+    do_sorting
+    total = do_calculation.round
   end
 
-  def extract_array( val )
+  private
+
+  attr_accessor :char_list, :num_list, :splitted_str
+
+  # Extracting the letter and number
+  # in each splitted string
+  # ex. str = '820b' become
+  #     char_list = [`b`]
+  #     num_list  = [`820]
+  def extract_str( val )
     if val =~ /\d/
       letter = val.gsub(/[^a-zA-Z]/, '')
-      @letter << letter
-      @num << extract_number( val, letter )
+      char_list << letter
+      num_list << extract_number( val, letter )
     end
   end
 
+  # Do sorting based on the list of character
+  # ex. char_list [ `z`, `b`, `a` ]
+  #     num_list  [ `456`, `736`, `001`]
+  #     --- AFTER SORTING --
+  #     char_list [ `a`, `b`, `z` ]
+  #     num_list [ `001`, `736`, `456` ]
   def do_sorting
-    array_length = @letter.length
+    char_list_length = char_list.length
 
     loop do
       swapped = false
-      (array_length - 1).times do | i |
-        if @letter[i] > @letter[i+1]
-          @letter[i], @letter[i+1] = @letter[i+1], @letter[i]
-          @num[i], @num[i+1] = @num[i+1], @num[i]
+      (char_list_length - 1).times do | i |
+        if char_list[i] > char_list[i+1]
+          char_list[i], char_list[i+1] = char_list[i+1], char_list[i]
+          num_list[i], num_list[i+1] = num_list[i+1], num_list[i]
 
           swapped = true
         end
@@ -49,6 +63,8 @@ class DoTheOperation
     end
   end
 
+  # Specific method where extracting the number
+  # in the string that has character
   def extract_number( val, letter )
     str_num = []
     val.split('').each do | num |
@@ -60,29 +76,29 @@ class DoTheOperation
     return str_num.join()
   end
 
+  # Do the calculation of the four
+  # basic operation in math
   def do_calculation
     index = 0
     total = 0.0
+    total_temp = 0
 
-    array_length = @num.length
+    num_list_length = num_list.length
 
-    ( array_length - 1 ).times do | i |
-      total = @num[i].to_f if i == 0
+    ( num_list_length - 1 ).times do | i |
+      total = num_list[i].to_f if i == 0
       index = 0 if index == 4
 
-      puts "OPERATIONS INDEX: #{index}"
-      puts "before ---> total: #{total} -- operation: #{OPERATIONS[index]} -- #{@num[i+1].to_f}"
-      total = total.send(OPERATIONS[index], @num[i+1].to_f).round
-      puts "TOTAL HERE: #{total}"
+      total = total.send(OPERATIONS[index], num_list[i+1].to_f)
       index = index + 1
     end
 
     total
   end
 
+  # A method for debugging purposes
   def output
-    puts "Letter sequence: #{@letter}"
-    puts "Number sequence: #{@num}"
+    puts "Letter sequence: #{@temp_letter}"
+    puts "Number sequence: #{@temp}"
   end
 end
-
