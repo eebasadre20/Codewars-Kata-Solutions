aa9d846d17731d188f2984437891438857642a39
eebasadre20 <eebasadre20@gmail.com>
2016-10-09 08:42:40 +0800

    Add problem link inside the solution.

--- week-1/is_narcissistic.rb
+++ week-1/is_narcissistic.rb
@@ -1,3 +1,7 @@
+# PROBLEM --> https://www.codewars.com/kata/numbers-so-vain-they-probably-think-this-kata-is-about-them/train/ruby
+# Click the link for more information
+# about the Kata problem.
+
 def is_narcissistic( *args )
   count = 0
 
@@ -5,8 +9,8 @@ def is_narcissistic( *args )
     return false if /\D/.match( num.to_s ) || num.to_s.empty?
 
     narcissistic =  num.to_s.split("").inject( 0 ) do | sum, i |
-              sum = sum + ( i.to_i ** num.to_s.size )
-            end == num.to_i ? true : false
+                      sum = sum + ( i.to_i ** num.to_s.size )
+                    end == num.to_i ? true : false
 
    count = count + 1 if narcissistic
   end
