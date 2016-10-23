da90542559794ca6dabeced1c594c9d286982643
Pepeng Agimat <eebasadre20@gmail.com>
2016-10-23 21:19:31 +0800

    Update to_camel_case.rb

--- week-3/to_camel_case.rb
+++ week-3/to_camel_case.rb
@@ -1,3 +1,4 @@
+https://www.codewars.com/kata/convert-string-to-camel-case/train/ruby
 def to_camel_case(str)
   arr_str = str.gsub(/\W+|_|\d/, " ").split(" ")
   arr_str.map { |s| s != arr_str[0] ? s.capitalize : s }.join("")
