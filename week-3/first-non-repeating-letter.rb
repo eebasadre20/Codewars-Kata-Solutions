fc9996c4b12dd575782ef1306c06bbe4d308229f
Pepeng Agimat <eebasadre20@gmail.com>
2016-10-23 22:18:59 +0800

    Update first-non-repeating-letter.rb

--- week-3/first-non-repeating-letter.rb
+++ week-3/first-non-repeating-letter.rb
@@ -3,28 +3,32 @@
 # about the Kata problem.
 
 def  first_non_repeating_letter(s)
-  # Code go here
   return s if s.empty?
-
+  puts s
+  
   str = s.split("").each_with_object(Hash.new(0)) do | word, count |
     count[word] += 1
   end
 
   str.each do |k, v|
-    if k.match( /\p{ Lower }/ )
+    if k != ',' && k.match(/\W+/)
+      str.delete(k)
+    end
+    
+    if k.match( /^[[:lower:]]+$/ )
       if str[k.upcase] > 0
         str.delete(k.upcase)
         str.delete( k )
-      elsif v > 1
-        str.delete( k )
       end
-    elsif k.match( /\p{ Upper }/ )
+    elsif k.match( /^[[:upper:]]+$/ )
       if str[k.downcase] > 0
         str.delete(k.downcase)
         str.delete( k )
-      elsif v > 1
-        str.delete( k )
       end
+    end
+    
+    if v > 1
+      str.delete( k )
     end
   end
 
