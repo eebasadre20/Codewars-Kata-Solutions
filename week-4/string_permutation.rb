0e48e8fe1bb630ef58d556e43a6e730236a044a9
eebasadre <edsil@piktochart.com>
2016-10-31 10:46:02 +0800

    Add link to the problem from codewars

--- week-4/string_permutation.rb
+++ week-4/string_permutation.rb
@@ -1,3 +1,18 @@
+require 'pry'
+# PROBLEM --> https://www.codewars.com/kata/permutations/train/ruby
+# Click the link for more information
+# about the Kata problem.
+
 def permutations(string)
+  # In here, we need to split the string pass to make an array of characters
+  # then use the permutation method from array module. The result of the permutated array
+  # will be map then join and remove the duplicate using uniq.
+  #
+  # Ex. "aabb".split.permutation.map(&:join).uniq
+  #
+  # [["a", "a", "b", "b"],["a", "b", "a", "b"],["b", "b", "a", "a"] .... ] - before the map and join
+  # ["aabb","abab","bbaa", ....] - after map and join
   string.split("").permutation.map(&:join).uniq
 end
+
+permutations("aabb")
