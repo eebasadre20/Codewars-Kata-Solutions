9bede38038a1ffbfb3ebd82ba54c59c3e4788cd9
eebasadre <edsil@piktochart.com>
2016-11-05 20:16:00 +0800

    Solution for break the camel case

---
+++ week-5/dont_drink_the_water.rb
@@ -0,0 +1,13 @@
+require 'pry'
+
+# Not yet finish
+def separate_liquids(glass)
+  density_chart = {
+    :'H' => 3,
+    :'W' => 2,
+    :'A' => 1,
+    :'O' =>0
+  }
+
+end
+separate_liquids([['H', 'H', 'W', 'O'],['W','W','O','W'],['H','H','O','O']])
