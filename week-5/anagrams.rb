01bcfcca09ed5624e95962c865905008284fff4e
eebasadre <edsil@piktochart.com>
2016-11-07 15:29:47 +0800

    Add comment to each solution

--- week-5/anagrams.rb
+++ week-5/anagrams.rb
@@ -3,11 +3,15 @@ require 'pry'
 def anagrams(word, words)
   flag = false
 
+  # check if the word includes in the list
   if !words.include? word
     words << word
     flag = true
   end
 
+  # Group in each sorted characters and return value
+  # then if the word is in the list delete it
+  # else return the list
   words.group_by { | word | word.chars.sort }.values
     .select do | w |
       if w.include?( word ) && flag == true
--- week-5/break_the_camel_case.rb
+++ week-5/break_the_camel_case.rb
@@ -1,5 +1,9 @@
 require 'pry'
 
 def solution(string)
+  # split the string into array of characters
+  # then map to check in which part has an uppercase letter
+  # if there an uppercase letter add space before the letter and join
+  # else just join directly.
   string.chars.map { | i |  i.match(/[A-Z]/) ? " " + i : i }.join("")
 end
